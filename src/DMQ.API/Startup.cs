f7f435840ecd3d21703aff5a4932003681650659
Shawn Shi <ss.shawnshi@gmail.com>
2021-04-15 23:05:48 -0400

    Rename API project

--- src/DMQ.API/Startup.cs
+++ src/DMQ.API/Startup.cs
@@ -11,7 +11,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace API
+namespace DMQ.API
 {
     public class Startup
     {
--- src/DMQ.API/WeatherForecast.cs
+++ src/DMQ.API/WeatherForecast.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace API
+namespace DMQ.API
 {
     public class WeatherForecast
     {
