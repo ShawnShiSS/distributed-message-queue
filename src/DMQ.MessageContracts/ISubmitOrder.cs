70b241db492b083eacbc29bdb54cff4719ccdeed
Shawn Shi <ss.shawnshi@gmail.com>
2021-04-17 13:30:50 -0400

    Contracts: add comment.

--- src/DMQ.MessageContracts/ISubmitOrder.cs
+++ src/DMQ.MessageContracts/ISubmitOrder.cs
@@ -2,6 +2,10 @@
 
 namespace DMQ.MessageContracts
 {
+    /// <summary>
+    ///     Contract for SubmitOrder.
+    ///     Note: if Mass Transit is used, MT will create a dynamic internal backing implementation of this interface.
+    /// </summary>
     public interface ISubmitOrder
     {
         Guid OrderId { get; }
