d10a1881dd4c98746d300822d979d2d1a5853038
Shawn Shi <ss.shawnshi@gmail.com>
2021-04-17 16:09:18 -0400

    Services: initial attempt to add services using RabbitMQ

---
+++ src/DMQ.MessageServices/MassTransitConsoleHostedService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using MassTransit;
+using System.Threading;
+
+namespace DMQ.MessageServices
+{
+    partial class Program
+    {
+        public class MassTransitConsoleHostedService : IHostedService
+        {
+            private readonly IBusControl _bus;
+
+            public MassTransitConsoleHostedService(IBusControl bus)
+            {
+                _bus = bus;
+            }
+
+            public async Task StartAsync(CancellationToken cancellationToken)
+            {
+                await _bus.StartAsync(cancellationToken).ConfigureAwait(false);
+            }
+
+            public async Task StopAsync(CancellationToken cancellationToken)
+            {
+                await _bus.StopAsync(cancellationToken);
+            }
+        }
+    }
+}
--- src/DMQ.MessageServices/Program.cs
+++ src/DMQ.MessageServices/Program.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿using DMQ.MessageComponents.Consumers;
+using MassTransit;
+using MassTransit.Definition;
+using MassTransit.RabbitMqTransport;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DMQ.MessageServices
 {
@@ -6,11 +18,56 @@ namespace DMQ.MessageServices
     ///     Program.
     ///     Note this will .NET Core Generic Host, which makes it easy to run the console app as a Windows Service or even Linux Demon.
     /// </summary>
-    class Program
+    partial class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var isService = !(Debugger.IsAttached) || args.Contains("--console");
+
+            var builder = new HostBuilder()
+                .ConfigureAppConfiguration((hostingContext, config) => 
+                {
+                    config.AddJsonFile("appsettings.json", true);
+                    config.AddEnvironmentVariables();
+
+                    if (args != null)
+                    {
+                        config.AddCommandLine(args);
+                    }
+                })
+                .ConfigureServices((hostingContext, services) => 
+                {
+                    // Use snake-like-names for queues
+                    services.TryAddSingleton(KebabCaseEndpointNameFormatter.Instance);
+                    services.AddMassTransit(cfg => 
+                    {
+                        // Allow Mass Transit to scan all the types instead of manually adding them all
+                        cfg.AddConsumersFromNamespaceContaining<SubmitOrderConsumer>();
+
+                        cfg.UsingRabbitMq(ConfigureBus);
+                    });
+
+                    services.AddHostedService<MassTransitConsoleHostedService>();
+                })
+                .ConfigureLogging((hostingContext, logging) => 
+                {
+                    logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                });
+            
+            if (isService)
+            {
+                await builder.UseWindowsService().Build().RunAsync();
+            }
+            else
+            {
+                await builder.RunConsoleAsync();
+            }
+        }
+
+        static void ConfigureBus(IBusRegistrationContext context, IRabbitMqBusFactoryConfigurator configurator)
+        {
+            configurator.ConfigureEndpoints(context);
         }
     }
 }
