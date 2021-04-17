cb66c0255c653dd6d503df447969fc8677eb4b91
Shawn Shi <ss.shawnshi@gmail.com>
2021-04-17 15:10:43 -0400

    Add rejected contract and validation in consumer.

--- src/DMQ.MessageComponents/Consumers/SubmitOrderConsumer.cs
+++ src/DMQ.MessageComponents/Consumers/SubmitOrderConsumer.cs
@@ -1,19 +1,51 @@
 ﻿using DMQ.MessageContracts;
 using MassTransit;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace DMQ.MessageComponents.Consumers
 {
+    /// <summary>
+    ///     Consumer for ISubmitOrder message.
+    ///     Note: all consumers are registered as scoped with the DI container.
+    /// </summary>
     public class SubmitOrderConsumer : IConsumer<ISubmitOrder>
     {
+        private readonly ILogger<SubmitOrderConsumer> _logger;
+
+        public SubmitOrderConsumer(ILogger<SubmitOrderConsumer> logger)
+        {
+            _logger = logger;
+        }
+
         public async Task Consume(ConsumeContext<ISubmitOrder> context)
         {
+            _logger.LogInformation($"SubmitOrderConsumer for customer number: {context.Message.CustomerNumber}");
+
+            if (!IsValidOrder(context.Message.CustomerNumber))
+            {
+                await context.RespondAsync<IOrderSubmissionRejected>(new 
+                {
+                    OrderId = context.Message.OrderId,
+                    Timestamp = InVar.Timestamp,
+                    CustomerNumber = context.Message.CustomerNumber,
+                    Reason = $"Customer can not be Test: {context.Message.CustomerNumber}"
+                });
+
+                return;
+            }
+
             await context.RespondAsync<IOrderSubmissionAccepted>(new
             {
                 OrderId = context.Message.OrderId,
                 Timestamp = InVar.Timestamp,
                 CustomerNumber = context.Message.CustomerNumber
             });
+        }
+
+        private bool IsValidOrder(string customerNumber)
+        {
+            return !customerNumber.Contains("TEST");
         }
     }
 }
---
+++ src/DMQ.MessageContracts/IOrderSubmissionRejected.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DMQ.MessageContracts
+{
+    public interface IOrderSubmissionRejected
+    {
+        Guid OrderId { get; }
+        DateTime Timestamp { get; }
+        string CustomerNumber { get; }
+        string Reason { get; }
+
+    }
+}
