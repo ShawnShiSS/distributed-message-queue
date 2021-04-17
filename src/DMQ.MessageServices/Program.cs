9c4099422e68d7226cfd0293225806e2b96c8c4d
Shawn Shi <ss.shawnshi@gmail.com>
2021-04-17 15:28:22 -0400

    Services: comment.

--- src/DMQ.MessageServices/Program.cs
+++ src/DMQ.MessageServices/Program.cs
@@ -2,7 +2,10 @@
 
 namespace DMQ.MessageServices
 {
-   
+    /// <summary>
+    ///     Program.
+    ///     Note this will .NET Core Generic Host, which makes it easy to run the console app as a Windows Service or even Linux Demon.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
