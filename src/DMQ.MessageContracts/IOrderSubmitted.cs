1c0f7410f83e99bea6372adf2e39e34fc0d8e872
Shawn Shi <ss.shawnshi@gmail.com>
2021-04-27 21:37:18 -0400

    Contract: add order submitted event contract.

---
+++ src/DMQ.MessageContracts/IOrderSubmitted.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DMQ.MessageContracts
+{
+    /// <summary>
+    ///     Order submitted event contract.
+    /// </summary>
+    public interface IOrderSubmitted
+    {
+        /// <summary>
+        ///     Unique identifier for an order
+        /// </summary>
+        Guid OrderId { get; }
+        DateTime Timestamp { get; }
+        string CustomerNumber { get; }
+    }
+}
