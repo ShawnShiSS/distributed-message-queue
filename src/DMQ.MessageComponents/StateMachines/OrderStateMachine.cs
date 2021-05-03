90e2ba62a18dec494e4c62109536eb83b1122af4
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-02 21:58:35 -0400

    Component: added state machine

--- src/DMQ.MessageComponents/StateMachines/OrderStateMachine.cs
+++ src/DMQ.MessageComponents/StateMachines/OrderStateMachine.cs
@@ -1,5 +1,6 @@
 ﻿using Automatonymous;
 using DMQ.MessageContracts;
+using MassTransit;
 using MassTransit.Saga;
 using System;
 
@@ -11,14 +12,67 @@ namespace DMQ.MessageComponents.StateMachines
         {
             // State machine events have to be correlated to the state machine instance.
             // E.g., OrderId in the OrderSubmitted event will be used to correlate the event to a saga instance. If the instance does not exist, it will get created.
+            // If Redis is used as the state machine repository, redis-cli allows us to check an order state by "get orderId"
             Event(() => OrderSubmitted, x => x.CorrelateById(m => m.Message.OrderId));
+            Event(() => CheckOrderStatus, x =>
+            {
+                x.CorrelateById(m => m.Message.OrderId);
+                // If state instance does not exist for the order id, we need to return not found.
+                x.OnMissingInstance(m => m.ExecuteAsync(async context => 
+                { 
+                    // Only respond if a response is expected
+                    if (context.RequestId.HasValue)
+                    {
+                        await context.RespondAsync<IOrderNotFound>(new 
+                        { 
+                            OrderId = context.Message.OrderId
+                        });
+                    }
+                }));
+            });
 
             InstanceState(x => x.CurrentState);
 
             // All state machines start in the initial state
+            // When an OrderSubmitted event is published, OrderStateMachine uses it to create an instance of itself, identified by order id.
             Initially(
                 When(OrderSubmitted)
+                    // Assign the properties in the message (in Data) to the saga instance
+                    .Then(context => 
+                    {
+                        context.Instance.CustomerNumber = context.Data.CustomerNumber;
+                        context.Instance.Updated = DateTime.UtcNow;
+                        context.Instance.SubmittedDate = context.Data.Timestamp;
+
+                    })
                     .TransitionTo(Submitted)
+            );
+
+            // Messages are not guaranteed to be processed in order, so we need to handle out-of-order messages.
+            // DuringAny includes all states other than initial state and final state.
+            DuringAny(
+                When(OrderSubmitted)
+                    .Then(context =>
+                    {
+                        context.Instance.CustomerNumber = context.Data.CustomerNumber;
+                        context.Instance.SubmittedDate = context.Data.Timestamp;
+
+                    })
+            );
+
+            // Handle check order status requests.
+            DuringAny(
+                When(CheckOrderStatus)
+                    .RespondAsync(x => x.Init<IOrderStatus>(new
+                    {
+                        OrderId = x.Instance.CorrelationId,
+                        State = x.Instance.CurrentState
+                    }))
+            );
+
+            During(Submitted,
+                // If the same order id gets submitted twice, the second request will fail with "Not accepted in state Submitted".
+                Ignore(OrderSubmitted)
             );
 
             // Final state is specical, as state machines in final state will be removed
@@ -33,6 +87,11 @@ namespace DMQ.MessageComponents.StateMachines
         ///     On order submitted event.
         /// </summary>
         public Event<IOrderSubmitted> OrderSubmitted { get; private set; }
+
+        /// <summary>
+        ///     On check order status event.
+        /// </summary>
+        public Event<ICheckOrder> CheckOrderStatus { get; private set; }
     }
 
     public class OrderState : 
@@ -45,14 +104,30 @@ namespace DMQ.MessageComponents.StateMachines
         public Guid CorrelationId { get; set; }
 
         /// <summary>
+        ///     Redis version check at run time.
+        ///     Redis uses optimistic concurrency and requires this.
+        /// </summary>
+        public int Version { get; set; }
+
+        /// <summary>
         ///     Current state of the saga instance.
         /// </summary>
         public string CurrentState { get; set; }
 
         /// <summary>
-        ///     Redis version check at run time.
-        ///     Redis uses optimistic concurrency and requires this.
+        ///     Customer number.
         /// </summary>
-        public int Version { get; set; }
+        public string CustomerNumber { get; set; }
+
+        /// <summary>
+        ///     Order submitted date.
+        /// </summary>
+        public DateTime? SubmittedDate { get; set; }
+
+        /// <summary>
+        ///     Updated timestamp
+        /// </summary>
+        public DateTime Updated { get; set; }
+        
     }
 }
