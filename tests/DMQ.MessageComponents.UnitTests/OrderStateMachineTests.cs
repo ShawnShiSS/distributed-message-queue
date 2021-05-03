df2109e95ff4ad9b1d2b3b18a37f6d51d4e0a4ce
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-02 22:01:05 -0400

    Unit Test: add unit tests.

---
+++ tests/DMQ.MessageComponents.UnitTests/OrderStateMachineTests.cs
@@ -0,0 +1,97 @@
+﻿using DMQ.MessageComponents.StateMachines;
+using DMQ.MessageContracts;
+using MassTransit.Testing;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMQ.MessageComponents.UnitTests
+{
+    [TestFixture]
+    public class OrderStateMachineTests
+    {
+        [Test]
+        public async Task Should_create_a_state_machine_instance()
+        {
+            // Mass Transit test harness has in-memory transport and in-memory saga repository
+            var harness = new InMemoryTestHarness();
+            var orderStateMachine = new OrderStateMachine();
+            var saga = harness.StateMachineSaga<OrderState, OrderStateMachine>(orderStateMachine);
+
+            await harness.Start();
+            try
+            {
+                Guid orderId = Guid.NewGuid();
+                string customerNumber = "123";
+
+                await harness.Bus.Publish<IOrderSubmitted>(new
+                {
+                    OrderId = orderId,
+                    Timestamp = DateTime.UtcNow,
+                    CustomerNumber = customerNumber
+                });
+                
+                // Expect a saga instance to be created
+                Assert.That(saga.Created.Select(x => x.CorrelationId == orderId).Any(), Is.True);
+
+                // Avoid race condition.
+                var instanceId = await saga.Exists(orderId, x => x.Submitted);
+                Assert.That(instanceId, Is.Not.Null);
+
+                var instance = saga.Sagas.Contains(instanceId.Value);
+                Assert.That(instance.CustomerNumber, Is.EqualTo(customerNumber));
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+
+        }
+
+        [Test]
+        public async Task Should_response_to_check_status_request()
+        {
+            // Mass Transit test harness has in-memory transport and in-memory saga repository
+            var harness = new InMemoryTestHarness();
+            var orderStateMachine = new OrderStateMachine();
+            var saga = harness.StateMachineSaga<OrderState, OrderStateMachine>(orderStateMachine);
+
+            await harness.Start();
+            try
+            {
+                Guid orderId = Guid.NewGuid();
+                string customerNumber = "123";
+
+                await harness.Bus.Publish<IOrderSubmitted>(new
+                {
+                    OrderId = orderId,
+                    Timestamp = DateTime.UtcNow,
+                    CustomerNumber = customerNumber
+                });
+
+                Assert.That(saga.Created.Select(x => x.CorrelationId == orderId).Any(), Is.True);
+
+                // Avoid race condition.
+                var instanceId = await saga.Exists(orderId, x => x.Submitted);
+                Assert.That(instanceId, Is.Not.Null);
+
+                var instance = saga.Sagas.Contains(instanceId.Value);
+                Assert.That(instance.CustomerNumber, Is.EqualTo(customerNumber));
+
+                var resquestClient = await harness.ConnectRequestClient<ICheckOrder>();
+                var response = await resquestClient.GetResponse<IOrderStatus>(new 
+                {
+                    OrderId = orderId
+                });
+
+                Assert.That(response.Message.State, Is.EqualTo(orderStateMachine.Submitted.Name));
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+
+        }
+    }
+}
---
+++ tests/DMQ.MessageComponents.UnitTests/SubmitOrderConsumerTest.cs
@@ -0,0 +1,141 @@
+﻿using DMQ.MessageComponents.Consumers;
+using DMQ.MessageContracts;
+using MassTransit;
+using MassTransit.Testing;
+using NUnit.Framework;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMQ.MessageComponents.UnitTests
+{
+    [TestFixture]
+    public class SubmitOrderConsumerTest
+    {
+        [Test]
+        public async Task Should_consume_submit_order_command()
+        {
+            // Mass Transit test harness has in-memory transport and in-memory saga repository
+            var harness = new InMemoryTestHarness();
+            var consumer = harness.Consumer<SubmitOrderConsumer>();
+
+            await harness.Start();
+            try
+            {
+                Guid orderId = Guid.NewGuid();
+                string customerNumber = "123";
+
+                await harness.InputQueueSendEndpoint.Send<ISubmitOrder>(new
+                {
+                    OrderId = orderId,
+                    Timestamp = DateTime.UtcNow,
+                    CustomerNumber = customerNumber
+                });
+
+                Assert.That(consumer.Consumed.Select<ISubmitOrder>().Any(), Is.True);
+                // Since message is sent and no response is expected
+                Assert.That(harness.Sent.Select<IOrderSubmissionAccepted>().Any(), Is.False);
+                Assert.That(harness.Sent.Select<IOrderSubmissionRejected>().Any(), Is.False);
+
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+
+        }
+
+        [Test]
+        public async Task Should_respond_with_acceptance_if_ok()
+        {
+            // Mass Transit test harness has in-memory transport and in-memory saga repository
+            var harness = new InMemoryTestHarness();
+            var consumer = harness.Consumer<SubmitOrderConsumer>();
+
+            await harness.Start();
+            try 
+            {
+                Guid orderId = Guid.NewGuid();
+                string customerNumber = "123";
+
+                var requestClient = await harness.ConnectRequestClient<ISubmitOrder>();
+                var response = await requestClient.GetResponse<IOrderSubmissionAccepted>(new
+                {
+                    OrderId = orderId,
+                    Timestamp = DateTime.UtcNow,
+                    CustomerNumber = customerNumber
+                });
+
+                Assert.That(response.Message.OrderId, Is.EqualTo(orderId));
+                Assert.That(consumer.Consumed.Select<ISubmitOrder>().Any(), Is.True);
+                Assert.That(harness.Sent.Select<IOrderSubmissionAccepted>().Any(), Is.True);
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+
+        }
+
+        [Test]
+        public async Task Should_respond_with_rejected_for_test_customer()
+        {
+            // Mass Transit test harness has in-memory transport and in-memory saga repository
+            var harness = new InMemoryTestHarness();
+            var consumer = harness.Consumer<SubmitOrderConsumer>();
+
+            await harness.Start();
+            try
+            {
+                Guid orderId = Guid.NewGuid();
+                string customerNumber = "TEST";
+
+                var requestClient = await harness.ConnectRequestClient<ISubmitOrder>();
+                var response = await requestClient.GetResponse<IOrderSubmissionRejected>(new
+                {
+                    OrderId = orderId,
+                    Timestamp = DateTime.UtcNow,
+                    CustomerNumber = customerNumber
+                });
+
+                Assert.That(response.Message.OrderId, Is.EqualTo(orderId));
+                Assert.That(consumer.Consumed.Select<ISubmitOrder>().Any(), Is.True);
+                Assert.That(harness.Sent.Select<IOrderSubmissionRejected>().Any(), Is.True);
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+
+        }
+
+        [Test]
+        public async Task Should_publish_order_submitted_event()
+        {
+            // Mass Transit test harness has in-memory transport and in-memory saga repository
+            var harness = new InMemoryTestHarness();
+            var consumer = harness.Consumer<SubmitOrderConsumer>();
+
+            await harness.Start();
+            try
+            {
+                Guid orderId = Guid.NewGuid();
+                string customerNumber = "123";
+
+                await harness.InputQueueSendEndpoint.Send<ISubmitOrder>(new
+                {
+                    OrderId = orderId,
+                    Timestamp = DateTime.UtcNow,
+                    CustomerNumber = customerNumber
+                });
+
+                Assert.That(harness.Published.Select<IOrderSubmitted>().Any(), Is.True);
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+
+        }
+    }
+}
