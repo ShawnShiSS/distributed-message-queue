193d9231b54e64375bff4a9f44693dd4dd403654
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-03 22:55:04 -0400

    Add new contract for customer account closed.

---
+++ src/DMQ.MessageContracts/ICustomerAccountClosed.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DMQ.MessageContracts
+{
+    public interface ICustomerAccountClosed
+    {
+        /// <summary>
+        ///     Customer Id.
+        /// </summary>
+        Guid CustomerId { get; }
+        /// <summary>
+        ///     Business identifier, customer number.
+        /// </summary>
+        string CustomerNumber { get; }
+    }
+}
