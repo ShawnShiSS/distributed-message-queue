ca97b74662d052364a564099de93eaeb11c76ba9
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-03 22:55:29 -0400

    API: new controller for customer.

---
+++ src/DMQ.API/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using DMQ.MessageContracts;
+using MassTransit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMQ.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CustomerController : ControllerBase
+    {
+       
+        private readonly ILogger<CustomerController> _logger;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public CustomerController(ILogger<CustomerController> logger,
+                                  IPublishEndpoint publishEndpoint)
+        {
+            _logger = logger;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> Delete(Guid id, string customerNumber)
+        {
+            await _publishEndpoint.Publish<ICustomerAccountClosed>(new
+            {
+                CustomerId = id,
+                CustomerNumber = customerNumber
+            });
+
+            return Ok();
+        }
+    }
+}
