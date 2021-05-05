b52171d4b0b2d26f687d020168a6ff79e262c1a5
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-04 22:33:59 -0400

    More Unit tests

--- tests/DMQ.MessageComponents.UnitTests/OrderStateMachineTests.cs
+++ tests/DMQ.MessageComponents.UnitTests/OrderStateMachineTests.cs
@@ -1,4 +1,6 @@
-﻿using DMQ.MessageComponents.StateMachines;
+﻿using Automatonymous.Graphing;
+using Automatonymous.Visualizer;
+using DMQ.MessageComponents.StateMachines;
 using DMQ.MessageContracts;
 using MassTransit.Testing;
 using NUnit.Framework;
@@ -76,9 +78,6 @@ namespace DMQ.MessageComponents.UnitTests
                 var instanceId = await saga.Exists(orderId, x => x.Submitted);
                 Assert.That(instanceId, Is.Not.Null);
 
-                var instance = saga.Sagas.Contains(instanceId.Value);
-                Assert.That(instance.CustomerNumber, Is.EqualTo(customerNumber));
-
                 var resquestClient = await harness.ConnectRequestClient<ICheckOrder>();
                 var response = await resquestClient.GetResponse<IOrderStatus>(new 
                 {
@@ -92,6 +91,111 @@ namespace DMQ.MessageComponents.UnitTests
                 await harness.Stop();
             }
 
+        }
+
+        [Test]
+        public async Task Should_cancel_when_customer_account_closed()
+        {
+            // Mass Transit test harness has in-memory transport and in-memory saga repository
+            var harness = new InMemoryTestHarness();
+            var orderStateMachine = new OrderStateMachine();
+            var saga = harness.StateMachineSaga<OrderState, OrderStateMachine>(orderStateMachine);
+
+            await harness.Start();
+            try
+            {
+                Guid orderId = Guid.NewGuid();
+                string customerNumber = "123";
+
+                await harness.Bus.Publish<IOrderSubmitted>(new
+                {
+                    OrderId = orderId,
+                    Timestamp = DateTime.UtcNow,
+                    CustomerNumber = customerNumber
+                });
+
+                Assert.That(saga.Created.Select(x => x.CorrelationId == orderId).Any(), Is.True);
+
+                // Avoid race condition.
+                var instanceId = await saga.Exists(orderId, x => x.Submitted);
+                Assert.That(instanceId, Is.Not.Null);
+
+                // Close account
+                await harness.Bus.Publish<ICustomerAccountClosed>(new
+                {
+                    CustomerId = orderId,
+                    CustomerNumber = customerNumber
+                });
+
+                // Avoid race condition.
+                instanceId = await saga.Exists(orderId, x => x.Canceled);
+                Assert.That(instanceId, Is.Not.Null);
+
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+
+        }
+
+        [Test]
+        public async Task Should_be_accepted_state_when_order_is_accepted()
+        {
+            // Mass Transit test harness has in-memory transport and in-memory saga repository
+            var harness = new InMemoryTestHarness();
+            var orderStateMachine = new OrderStateMachine();
+            var saga = harness.StateMachineSaga<OrderState, OrderStateMachine>(orderStateMachine);
+
+            await harness.Start();
+            try
+            {
+                Guid orderId = Guid.NewGuid();
+                string customerNumber = "123";
+
+                await harness.Bus.Publish<IOrderSubmitted>(new
+                {
+                    OrderId = orderId,
+                    Timestamp = DateTime.UtcNow,
+                    CustomerNumber = customerNumber
+                });
+
+                Assert.That(saga.Created.Select(x => x.CorrelationId == orderId).Any(), Is.True);
+
+                // Avoid race condition.
+                var instanceId = await saga.Exists(orderId, x => x.Submitted);
+                Assert.That(instanceId, Is.Not.Null);
+
+                // Accept
+                await harness.Bus.Publish<IOrderAccepted>(new
+                {
+                    OrderId = orderId
+                });
+
+                // Avoid race condition.
+                instanceId = await saga.Exists(orderId, x => x.Accepted);
+                Assert.That(instanceId, Is.Not.Null);
+
+            }
+            finally
+            {
+                await harness.Stop();
+            }
+
+        }
+
+        [Test]
+        public void Show_me_the_state_machine()
+        {
+            var orderStateMachine = new OrderStateMachine();
+
+            var graph = orderStateMachine.GetGraph();
+
+            var generator = new StateMachineGraphvizGenerator(graph);
+
+            string dots = generator.CreateDotFile();
+
+            Console.WriteLine(dots);
         }
     }
 }
