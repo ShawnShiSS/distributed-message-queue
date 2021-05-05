db6576207d1d1a5a4c4bb8e5c94151ce02b430c6
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-04 22:56:10 -0400

    Quick comment.

--- src/DMQ.MessageComponents/StateMachines/OrderStateMachine.cs
+++ src/DMQ.MessageComponents/StateMachines/OrderStateMachine.cs
@@ -68,6 +68,7 @@ namespace DMQ.MessageComponents.StateMachines
 
                     }),
                 When(OrderAccepted)
+                    // custom activity
                     .Activity(x => x.OfType<AcceptOrderActivity>())
                     .TransitionTo(Accepted)
             );
