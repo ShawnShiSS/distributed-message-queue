22bdda7e35dd52d65e99364bdb056ada8690cd85
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-05 21:51:59 -0400

    Warehouse: consumers.

---
+++ src/Warehouse.MessageComponents/AllocateInventoryConsumer.cs
@@ -0,0 +1,24 @@
+﻿using MassTransit;
+using System.Threading.Tasks;
+using Warehouse.MessageContracts;
+
+namespace Warehouse.MessageComponents
+{
+    public class AllocateInventoryConsumer
+        : IConsumer<IAllocateInventory>
+    {
+        public async Task Consume(ConsumeContext<IAllocateInventory> context)
+        {
+            // Fake system-specific allocation workflow.
+            await Task.Delay(500);
+
+            // Return response.
+            await context.RespondAsync<IInventoryAllocated>(new
+            {
+                AllocationId = context.Message.AllocationId,
+                ItemNumber = context.Message.ItemNumber,
+                Quantity = context.Message.Quantity
+            });
+        }
+    }
+}
