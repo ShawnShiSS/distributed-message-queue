55dd8272701b3d6e48790780d5742a436a9ac31c
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-06 22:12:00 -0400

    Warehouse service: initial setup and the routing slip runs end-to-end!!

---
+++ src/Warehouse.MessageServices/MassTransitConsoleHostedService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+using MassTransit;
+using System.Threading;
+
+namespace Warehouse.MessageServices
+{
+    public class MassTransitConsoleHostedService : IHostedService
+    {
+        private readonly IBusControl _bus;
+
+        public MassTransitConsoleHostedService(IBusControl bus)
+        {
+            _bus = bus;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            await _bus.StartAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            await _bus.StopAsync(cancellationToken);
+        }
+    }
+}
