9e01b63eaa4a2966376b9b7b71e3a6f008038ec8
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-09 14:04:33 -0400

    API: update endpoint conventions.

---
+++ src/DMQ.API/Models/Order/Create.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DMQ.API.Models.Order
+{
+    public class Create
+    {
+        public Guid OrderId { get; set; }
+        public string CustomerNumber { get; set; }
+    }
+}
