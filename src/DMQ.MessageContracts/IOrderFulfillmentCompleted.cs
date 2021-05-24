ebed8990461bdcbeebed6d0384e0a9968193cd50
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-23 23:05:05 -0400

    Contract: order fulfillment.

---
+++ src/DMQ.MessageContracts/IOrderFulfillmentCompleted.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DMQ.MessageContracts
+{
+    public interface IOrderFulfillmentCompleted
+    {
+        Guid OrderId { get; }
+
+        DateTime Timestamp { get; }
+    }
+}
---
+++ src/DMQ.MessageContracts/IOrderFulfillmentFaulted.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DMQ.MessageContracts
+{
+    public interface IOrderFulfillmentFaulted
+    {
+        Guid OrderId { get; }
+        DateTime TimeStamp { get; }
+    }
+
+
+}
