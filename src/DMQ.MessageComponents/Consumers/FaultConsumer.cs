1994222af6661f9369bd2f191a5433448f534a8e
Shawn Shi <ss.shawnshi@gmail.com>
2021-05-28 21:47:07 -0400

    Consumer for faulted messages.

---
+++ src/DMQ.MessageComponents/Consumers/FaultConsumer.cs
@@ -0,0 +1,21 @@
+﻿using DMQ.MessageContracts;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMQ.MessageComponents.Consumers
+{
+    /// <summary>
+    ///     Consumer for faulted messages.
+    /// </summary>
+    public class FaultConsumer :
+        IConsumer<Fault<IFulfillOrder>>
+    {
+        public Task Consume(ConsumeContext<Fault<IFulfillOrder>> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
